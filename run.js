7b6cf15f5ee019c4eb74104da9e588939f15ebf2
shivamgupta2020 <shivamgupta1968@gmail.com>
2024-04-22 16:38:47 +0530

    changed formatting

--- run.js
+++ run.js
@@ -1,12 +1,14 @@
 const blockHeader = require('./mine_block.js');
 const coinbase = require('./coinbase_transaction.js');
 const txids = require('./txids_generator.js');
+const {bigToLittleEndian} = require('./utils.js');
 
 const fs = require('fs');
 
 // Sample array
 const array1 = [blockHeader, coinbase];
-const array2 = txids;
+//convert each element of the txids array to a little endian
+const array2 = txids.map(txid => bigToLittleEndian(txid));
 const array = array1.concat(array2);
 // File path
 const filePath = 'output.txt';
