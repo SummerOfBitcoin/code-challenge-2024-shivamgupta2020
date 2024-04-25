f46cd4792139432e9c51bbe75ecfdea3f5a67842
shivamgupta2020 <shivamgupta1968@gmail.com>
2024-04-26 00:46:48 +0530

    changed hash

--- blockHeader.js
+++ blockHeader.js
@@ -1,13 +1,10 @@
 const {merkleRootTxid} = require('./create_merkle_txid.js')
 const { decimalToLittleEndian8, bigToLittleEndian } = require('./utils.js');
-const {hexTime_reverse} = require('./check.js')
-// const currentTimeInSeconds = Math.floor(Date.now() / 1000);
 
-// // Convert to hexadecimal
-// const hexTime = currentTimeInSeconds.toString(16);
-// const {hexTime_reverse} = bigToLittleEndian(hexTime)
-// console.log(hexTime_reverse)
-//000000200000000000000000000000000000000000000000000000000000000000000000f4c4dd3bad0777712bd9d657991f33185831d9a59f4757cc30346496bf7453e33c612666ffff001f92ee0000
+const currentTimeInSeconds = Math.floor(Date.now() / 1000);
+const hexTime = currentTimeInSeconds.toString(16);
+const hexTime_reverse = bigToLittleEndian(hexTime)
+
 const block = {
     version: 2,//VERSION
     previousBlockHash: "0000000000000000000000000000000000000000000000000000000000000000",
@@ -16,13 +13,13 @@ const block = {
     difficultyTarget: "0000ffff00000000000000000000000000000000000000000000000000000000",
     nonce: 0
 }
-
+console.log(block)
 
 function createBlockHeader(x){
     let blockheader = ""
     blockheader += decimalToLittleEndian8(x.version);
     blockheader += (x.previousBlockHash);
-    blockheader += (x.merkleRootHash);
+    blockheader += bigToLittleEndian(x.merkleRootHash);
     blockheader += x.timeStamp;
     blockheader += decimalToLittleEndian8("1f00ffff")
     return blockheader;
--- mine_block.js
+++ mine_block.js
@@ -1,5 +1,5 @@
 const {blockHeaderwithoutnounce, block} = require('./blockHeader.js');
-const { decimalToLittleEndian8, hash256 } = require('./utils.js');
+const { decimalToLittleEndian8,bigToLittleEndian, hash256 } = require('./utils.js');
 
 
 // Function to mine the block
@@ -9,9 +9,10 @@ function mineBlock() {
         // Combine block header and nonce for hashing
         const data = blockHeaderwithoutnounce + decimalToLittleEndian8(nonce);
         // Calculate hash of the block header and nonce
-        const hash = hash256(data);
+        const hash = bigToLittleEndian(hash256(data));
         // Check if hash meets the difficulty target
         if (hash < block.difficultyTarget) {
+            console.log(`Block mined with nonce: ${data}`);
             return data; // Return the valid nonce
         }
         nonce++; // Increment nonce for next attempt
